7a7f78319f91d17cc9747703148bc7df045e3bb7
Karel Bilek <kb@karelbilek.com>
2017-12-12 23:23:23 +0100

    Using require for json

--- src/lowlevel/protobuf/parse_protocol.js
+++ src/lowlevel/protobuf/parse_protocol.js
@@ -7,7 +7,7 @@
 import * as ProtoBuf from "protobufjs-old-fixed-webpack";
 
 import {Messages} from "./messages.js";
-import messagesJson from "./messages.json";
+const messagesJson = require(`./messages.json`);
 
 // Parse configure data (it has to be already verified)
 export function parseConfigure(data: Buffer): Messages {
