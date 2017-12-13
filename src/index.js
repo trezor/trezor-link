8a077ebfc0ad721a0afe6b550faaf43d11ad9f0a
Karel Bilek <kb@karelbilek.com>
2017-12-13 02:58:07 +0100

    Removing unnecessary cruft

--- src/index.js
+++ src/index.js
@@ -3,22 +3,19 @@
 // export is empty, you can import by "trezor-link/parallel", "trezor-link/lowlevel", "trezor-link/bridge"
 
 export type {Transport, AcquireInput, TrezorDeviceInfoWithSession, MessageFromTrezor} from './transport';
-export type {LowlevelTransportPlugin} from './lowlevel/plugin';
 
 import BridgeTransport from './bridge';
-import LowlevelTransport from './lowlevel';
-import ParallelTransport from './parallel';
+import LowlevelTransportWithSharedConnections from './lowlevel/withSharedConnections';
 import FallbackTransport from './fallback';
-import ChromeUdpPlugin from './lowlevel/chrome-udp';
-import ChromeHidPlugin from './lowlevel/chrome-hid';
+import WebUsbPlugin from './lowlevel/webusb';
+
+import 'whatwg-fetch';
 
 BridgeTransport.setFetch(fetch);
 
 export default {
   Bridge: BridgeTransport,
-  Parallel: ParallelTransport,
   Fallback: FallbackTransport,
-  ChromeUdp: ChromeUdpPlugin,
-  ChromeHid: ChromeHidPlugin,
-  Lowlevel: LowlevelTransport,
+  Lowlevel: LowlevelTransportWithSharedConnections,
+  WebUsb: WebUsbPlugin,
 };
