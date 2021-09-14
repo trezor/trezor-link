eac9ac3b56a53020c98d5211136699d5ff0e3623
Martin Varmuza <mvarmuza@gmail.com>
2021-09-14 13:07:17 +0200

    new bridge entry point

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // export is empty, you can import by "trezor-link/parallel", "trezor-link/lowlevel", "trezor-link/bridge"
 
 import BridgeTransportV2 from "./bridge/v2";
+import BridgeTransportV2New from "./bridge/v2-new";
 import LowlevelTransportWithSharedConnections from "./lowlevel/withSharedConnections";
 import FallbackTransport from "./fallback";
 import WebUsbPlugin from "./lowlevel/webusb";
@@ -24,12 +25,15 @@ if (typeof window === `undefined`) {
   // eslint-disable-next-line quotes
   const fetch = require("node-fetch");
   BridgeTransportV2.setFetch(fetch, true);
+  BridgeTransportV2New.setFetch(fetch, true);
 } else {
   BridgeTransportV2.setFetch(fetch, false);
+  BridgeTransportV2New.setFetch(fetch, false);
 }
 
 export default {
   BridgeV2: BridgeTransportV2,
+  BridgeV2New: BridgeTransportV2New,
   Fallback: FallbackTransport,
   Lowlevel: LowlevelTransportWithSharedConnections,
   WebUsb: WebUsbPlugin,
