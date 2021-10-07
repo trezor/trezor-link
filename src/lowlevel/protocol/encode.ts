814da41a2e9e62fbd4167c99ae16864d01496526
Martin Varmuza <mvarmuza@gmail.com>
2021-10-07 13:05:23 +0200

    upgrade to typescript and refactor protobuf layer

---
+++ src/lowlevel/protocol/encode.ts
@@ -0,0 +1,59 @@
+import * as ByteBuffer from 'bytebuffer';
+
+import { HEADER_SIZE, MESSAGE_HEADER_BYTE, BUFFER_SIZE } from '../../config';
+
+type Options<Chunked> = {
+    chunked: Chunked;
+    addTrezorHeaders: boolean;
+    messageType: number;
+};
+
+function encode(data: Uint8Array, options: Options<true>): Buffer[];
+function encode(data: Uint8Array, options: Options<false>): Buffer;
+function encode(data: any, options: any): any {
+    const { addTrezorHeaders, chunked, messageType } = options;
+    const bytes: Uint8Array = new Uint8Array(data);
+    const fullSize = (addTrezorHeaders ? HEADER_SIZE : HEADER_SIZE - 2) + data.length;
+
+    const encodedByteBuffer = new ByteBuffer(fullSize);
+
+    if (addTrezorHeaders) {
+        // 2*1 byte
+        encodedByteBuffer.writeByte(MESSAGE_HEADER_BYTE);
+        encodedByteBuffer.writeByte(MESSAGE_HEADER_BYTE);
+    }
+
+    // 2 bytes
+    encodedByteBuffer.writeUint16(messageType);
+
+    // 4 bytes (so 8 in total)
+    encodedByteBuffer.writeUint32(bytes.length);
+
+    // then put in the actual message
+    encodedByteBuffer.append(bytes);
+
+    encodedByteBuffer.reset();
+
+    if (chunked === false) {
+        return encodedByteBuffer.buffer;
+    }
+
+    const result: Buffer[] = [];
+    const size = BUFFER_SIZE;
+
+    // How many pieces will there actually be
+    const count = Math.floor((encodedByteBuffer.buffer.length - 1) / size) + 1 || 1;
+
+    // slice and dice
+    for (let i = 0; i < count; i++) {
+        const start = i * size;
+        const end = Math.min((i + 1) * size, encodedByteBuffer.limit);
+        const slice = encodedByteBuffer.slice(start, end);
+        slice.compact();
+        result.push(slice.buffer);
+    }
+
+    return result;
+}
+
+export { encode };
